610b5496af4de2c237d20731c2a4c380cc7a4895
mpoprawa <mateusz.poprawa@gmail.com>
2021-08-27 11:17:11 +0200

    complete contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import './Contact.scss'
+
+import contact from '../../assets/images/contact.jpg'
+import { Image } from '../../components/UI'
 
 const Contact = () => {
-	return <div>Contact PAGE</div>
+	return (
+		<div className='contact'>
+			<div className='contact__img'>
+				<Image src={contact} alt='about us' />
+			</div>
+			<div className='contact__content'>
+				Nunc eget tempor enim. Suspendisse dignissim enim turpis, non lobortis
+				mauris elementum ut. Fusce vitae massa neque. Curabitur lobortis
+				elementum ligula, et vehicula erat condimentum eu. Proin mattis turpis
+				ut nibh viverra, at pretium nunc accumsan. Vestibulum ac risus libero.
+				<br />
+				<br />
+				Nunc eget tempor enim. Suspendisse dignissim enim turpis, non lobortis
+				mauris elementum ut. Fusce vitae massa neque. Curabitur lobortis
+				elementum ligula, et vehicula erat condimentum eu. Proin mattis turpis
+				ut nibh viverra, at pretium nunc accumsan. Vestibulum ac risus libero.
+			</div>
+		</div>
+	)
 }
 
 export default Contact
