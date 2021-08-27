9fca86c62f949448fc974dabffa52a5d5386d98a
mpoprawa <mateusz.poprawa@gmail.com>
2021-08-27 10:56:29 +0200

    complete posts page

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,57 @@
+import './Input.scss'
+
+import React from 'react'
+
+type InputProps = {
+	type: string
+	value: string
+	placeholder: string
+	input?: string
+	onChange: (
+		e:
+			| React.ChangeEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLTextAreaElement>
+	) => void
+	name: string
+	required?: boolean
+}
+
+const Input = ({
+	type,
+	value,
+	placeholder,
+	input,
+	onChange,
+	name,
+	required = false,
+}: InputProps) => {
+	return (
+		<>
+			{input === 'textarea' ? (
+				<textarea
+					data-test='component-input-textarea'
+					onChange={onChange}
+					className='input'
+					rows={5}
+					placeholder={placeholder}
+					name={name}
+					value={value}
+					required={required}
+				/>
+			) : (
+				<input
+					data-test='component-input-input'
+					onChange={onChange}
+					className='input'
+					type={type}
+					value={value}
+					placeholder={placeholder}
+					name={name}
+					required={required}
+				/>
+			)}
+		</>
+	)
+}
+
+export default Input
--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,7 +1,172 @@
+import './Posts.scss'
+
 import React from 'react'
+import { useState } from 'react'
+
+import { Input, Card } from '../../components/UI'
 
 const Posts = () => {
-	return <div>Posts PAGE</div>
+	const [filterPhrase, setFilterPhrase] = useState('')
+
+	const filterPostsHandler = (
+		e:
+			| React.ChangeEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLTextAreaElement>
+	) => {
+		setFilterPhrase(e.target.value.toLowerCase())
+	}
+
+	return (
+		<div className='posts'>
+			<div className='posts__search'>
+				<Input
+					type='text'
+					placeholder='search'
+					onChange={filterPostsHandler}
+					value={filterPhrase}
+					name='filter posts'
+				/>
+			</div>
+			<div className='posts__cards'>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+				<Card
+					img='https://via.placeholder.com/400x250'
+					post={{ title: 'some title', id: '5' }}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Posts
