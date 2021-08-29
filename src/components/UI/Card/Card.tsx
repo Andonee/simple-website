2ab5eca6881a85a809ac5d31fe03f4318d39b9a3
mpoprawa <mateusz.poprawa@gmail.com>
2021-08-29 06:36:27 +0200

    remove fav button

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom'
 type CardProps = {
 	post: {
 		title: string
-		id: string
+		id: number
 	}
 	img: string
 }
@@ -25,9 +25,6 @@ const Card = ({ post, img }: CardProps) => {
 		<div className='card' onClick={onCardClick}>
 			<img src={img} alt={title} className='card__img' />
 			<p>{title}</p>
-			<button id={id} className='card__button'>
-				<i className={`far fa-heart`}></i>
-			</button>
 		</div>
 	)
 }
